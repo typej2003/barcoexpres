58525f4624f41ed1b6f5a8f0129d3f2df880e6bf
typej2003 <typej2003@gmail.com>
2025-12-22 20:02:24 -0400

    efefe

--- resources/views/emails/admin-msj-invitacion.blade.php
+++ resources/views/emails/admin-msj-invitacion.blade.php
@@ -29,7 +29,9 @@
 
                     <tr>
                         <td style="padding: 40px 30px 20px 30px; text-align: center;">
-                            <img src="{{ url('storage/avatarspromociones/panexpres_navidad.jpg') }}" alt="Pan Express">
+                            <img src="{{ $message->embed(public_path('storage/avatarspromociones/panexpres_navidad.jpg')) }}" 
+     style="width: 100%; max-width: 600px;" 
+     alt="Promoción Navidad">
                             <hr style="width: 80%; border: 0; border-top: 5px solid #893e08; margin: 20px auto 0 auto; opacity: 1;">
                         </td>
                     </tr>
